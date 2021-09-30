98f3608d95ce0d58727e5641bc77708d32dc65f6
Anupriyam Shukla <60869367+Anupriyam-FOSD@users.noreply.github.com>
2021-09-30 19:00:01 +0530

    Add files via upload

---
+++ Average of numbers.py
@@ -0,0 +1,9 @@
+#Taking inputs
+
+value1=float(input("Enter the first nnumber :"))
+value2=float(input("Enter the second number :"))
+value3=float(input("Enter the third number :"))
+
+average=(value1+value2+value3)/3
+
+print("The average of the numbers is :", average)
